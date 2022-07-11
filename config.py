b9cea37250c09f6bd53afce77915e4708e764904
Syed Ali Tariq <alitariq.bsc06@gmail.com>
2022-07-11 17:32:16 +0500

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ parser.add_argument('--choose_subclass' ,default = False, type=str2bool)## choos
 
 parser.add_argument('--counterfactual_PP' ,default = True, type=str2bool)## whether to generate filters for PP  or PN case 
 parser.add_argument('--resume_counterfactual_net' ,default = True, type=str2bool)## False = train CF model from scratch; True = resume training CF model
-parser.add_argument('--resume_from_epoch' ,default = 3, type=np.int32)## False = train CF model from scratch; True = resume training CF model
+parser.add_argument('--resume_from_epoch' ,default = 6, type=np.int32)## False = train CF model from scratch; True = resume training CF model
 parser.add_argument('--test_counterfactual_net' ,default = False, type=str2bool)## 
 parser.add_argument('--load_counterfactual_net',default = True, type=str2bool)
 parser.add_argument('--resume', default =True, type=str2bool) # load saved weights for base model
@@ -43,7 +43,7 @@ parser.add_argument('--alter_class', default = 9, type = np.int32) # alter class
 parser.add_argument('--analysis_class', default = 9, type = np.int32) # class for which images are loaded and analyzed
 parser.add_argument('--find_global_filters', default = False, type=str2bool) # perform statistical analysis to find the activation magnitude of all filters for the alter class and train images of alter class
 #parser.add_argument('--alter_class_2', default = 0, type = np.int32) # alter class for 2nd example, 9, 170, 25, 125, 108
-parser.add_argument('--cfe_epochs', default = 6, type = np.int32 ) #100 for mnist, 200 for CUB
+parser.add_argument('--cfe_epochs', default = 9, type = np.int32 ) #100 for mnist, 200 for CUB
 parser.add_argument('--l1_weight', default = 2, type = np.float32) # 2 default
 parser.add_argument('--save_logFile', default = True, type=str2bool) #
 
